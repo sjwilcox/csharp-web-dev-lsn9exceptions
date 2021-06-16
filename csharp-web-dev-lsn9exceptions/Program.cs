f8b12e6e40aea2df8ab328a8d3143113cda64b2e
steven.j.wilcox@gmail.com <steven.j.wilcox@gmail.com>
2021-06-16 19:26:21 -0400

    new code

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 //Exercises for the Exceptions Chapter
 
@@ -7,21 +8,57 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
-        static double Divide(double x, double y)
+        
+        static void Divide(double x, double y)
         {
-            // Write your code here!
+            int zero = 0;
+            try
+            {
+                if(y == zero)
+                {
+                    throw new DivideByZeroException("You cannot divide by zero");
+                }
+                else
+                {
+                    double score = (x / y) * 100;
+                    Console.WriteLine(score);
+                }
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
-        static int CheckFileExtension(string fileName)
+        static void CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            
+            int point = 0;
+            if(fileName.Contains(".cs"))
+            {
+                point++;
+                Console.WriteLine($"Correct extension {point} point awarded.");
+            }
+            else
+            {
+                Console.WriteLine($"Incorrect file extension {point} points awarded.");
+            }
+
         }
-        */
+        
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            Console.WriteLine("Please enter possible points: ");
+            double possiblePoints = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("Please enter points achieved by student: ");
+            double studentPoints = Convert.ToDouble(Console.ReadLine());
+            
+
+            Divide(studentPoints,possiblePoints);
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +67,14 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
+            foreach(KeyValuePair <string,string> val in students)
+            {
+                
+                string stuFile = val.Value.ToString();
 
+                CheckFileExtension(stuFile);
+            }
+            
         }
     }
-}
+} 
